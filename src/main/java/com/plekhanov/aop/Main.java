dc603131ae8460142982309853ef233b66a4371f
alexandr <A.A.Plekhanov>
2018-12-14 19:29:34 +0300

    added xml config aop

--- src/main/java/com/plekhanov/aop/Main.java
+++ src/main/java/com/plekhanov/aop/Main.java
@@ -2,10 +2,13 @@ package com.plekhanov.aop;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(com.plekhanov.aop.MyConf.class);
+      //  ApplicationContext context = new AnnotationConfigApplicationContext(com.plekhanov.aop.MyConf.class);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:aspConfig.xml");
 
         MyRepo repo = context.getBean(MyRepo.class);
         repo.getInfo();
--- src/main/java/com/plekhanov/aop/MyTransactionAspect.java
+++ src/main/java/com/plekhanov/aop/MyTransactionAspect.java
@@ -4,27 +4,27 @@ package com.plekhanov.aop;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class MyTransactionAspect {
 
-    @Before("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
+  //  @Before("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
     public void beginTransaction(){
         System.out.println("begin transaction");
     }
-     @AfterThrowing("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
+   //  @AfterThrowing("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
     public void commitTransaction(){
          System.out.println("commit transaction");
     }
 
 
-    @After("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
+  //  @After("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
     public void after(){
         System.out.println("after transaction");
     }
 
 
-    @AfterReturning("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
+  //  @AfterReturning("execution(* com.plekhanov.aop.MyUserInfo.getInfo())")
     public void rollback(){
         System.out.println("rollback transaction");
     }
